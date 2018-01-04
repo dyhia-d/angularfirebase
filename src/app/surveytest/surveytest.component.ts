a5be323c7383b2456b458dc32021409e850c968b
dyhia-d <dyhiadib@gmail.com>
2018-01-04 20:28:03 +0100

    affichage JSON ok

--- src/app/surveytest/surveytest.component.ts
+++ src/app/surveytest/surveytest.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Survey } from '../survey.model';
+import { Component, Input, OnInit } from '@angular/core';
+import * as Survey_t from 'survey-angular';
 
 @Component({
   selector: 'app-surveytest',
@@ -7,18 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveytestComponent implements OnInit {
 
-  constructor() { }
-
-  json = { title: 'BANANA Feedback Survey Example', showProgressBar: 'top', pages: [
-    {
-      questions: [{
+  ngOnInit() {
+    const json = { title: 'Product Feedback Survey Example', showProgressBar: 'top', pages: [
+      {
+        questions: [{
           type: 'matrix',
           name: 'Quality',
           title: 'Please indicate if you agree or disagree with the following statements',
           columns: [{
-              value: 1,
-              text: 'Strongly Disagree'
-            },
+            value: 1,
+            text: 'Strongly Disagree'
+          },
             {
               value: 2,
               text: 'Disagree'
@@ -37,9 +38,9 @@ export class SurveytestComponent implements OnInit {
             }
           ],
           rows: [{
-              value: 'affordable',
-              text: 'Product is affordable'
-            },
+            value: 'affordable',
+            text: 'Product is affordable'
+          },
             {
               value: 'does what it claims',
               text: 'Product does what it claims'
@@ -53,31 +54,76 @@ export class SurveytestComponent implements OnInit {
               text: 'Product is easy to use'
             }
           ]
-        }
-      ]
-    }, {
-      questions: [{
+        },
+          {
+            type: 'rating',
+            name: 'satisfaction',
+            title: 'How satisfied are you with the Product?',
+            mininumRateDescription: 'Not Satisfied',
+            maximumRateDescription: 'Completely satisfied'
+          },
+          {
+            type: 'rating',
+            name: 'recommend friends',
+            visibleIf: '{satisfaction} > 3',
+            title: 'How likely are you to recommend the Product to a friend or co-worker?',
+            mininumRateDescription: 'Will not recommend',
+            maximumRateDescription: 'I will recommend'
+          },
+          {
+            type: 'comment',
+            name: 'suggestions',
+            title: 'What would make you more satisfied with the Product?',
+          }
+        ]
+      }, {
+        questions: [{
           type: 'radiogroup',
           name: 'price to competitors',
           title: 'Compared to our competitors, do you feel the Product is',
           choices: ['Less expensive', 'Priced about the same', 'More expensive', 'Not sure']
-        }
-      ]
-    }, {
-      questions: [{
-        type: 'text',
-        name: 'email',
-        title: 'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
+        },
+          {
+            type: 'radiogroup',
+            name: 'price',
+            title: 'Do you feel our current price is merited by our product?',
+            choices: ['correct|Yes, the price is about right',
+              'low|No, the price is too low for your product',
+              'high|No, the price is too high for your product'
+            ]
+          },
+          {
+            type: 'multipletext',
+            name: 'pricelimit',
+            title: 'What is the... ',
+            items: [{
+              name: 'mostamount',
+              title: 'Most amount you would every pay for a product like ours'
+            },
+              {
+                name: 'leastamount',
+                title: 'The least amount you would feel comfortable paying'
+              }
+            ]
+          }
+        ]
+      }, {
+        questions: [{
+          type: 'text',
+          name: 'email',
+          title: 'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
+        }]
       }]
-    }]
-};
+    };
+    let surveyModel = new Survey_t.ReactSurveyModel(json);
 
-onSurveySaved(survey) {
-  this.json = survey;
-  document.querySelector('#surveyResult').innerHTML = "result: " + JSON.stringify(survey);
-}
+    //var survey = new Survey_t.Model(json);
+    
+    surveyModel.onComplete.add(function(result) {
+      document.querySelector('#result').innerHTML = "result: " + JSON.stringify(result.data);
+    });
 
-  ngOnInit() {
+    Survey_t.SurveyNG.render('surveyElement', { model: surveyModel });
   }
-
 }
+
